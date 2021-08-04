4e83c313cd1bc9570ff5e634f5a643c20f59039d
gerrardlee29 <gerrardlee29@gmail.com>
2021-08-04 09:10:39 +0000

    Added function code for Map

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+function initMap() {
+    // Create the map.
+     const map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 7,
+         center: { lat: 52.632469, lng: -1.689423 },
+     });
+ 
+ }
