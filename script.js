aa7f33332e5d15c5df57580c971a8c77bfafce73
gerrardlee29 <gerrardlee29@gmail.com>
2021-08-07 00:32:06 +0000

    Fixed function marker code, added Dog Park specifics to both title and info window, created multiple markers for different park locations, add marker animation

--- script.js
+++ script.js
@@ -2,13 +2,42 @@
 
 
 
+    var map;
+    function initMap(){
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 38.881265, lng: -104.769180},
+        zoom: 14,
+      });
 
 
-function initMap() {
-    // Create the map.
-     const map = new google.maps.Map(document.getElementById('map'), {
-         zoom: 7,
-         center: { lat: 52.632469, lng: -1.689423 },
-     });
- 
- }
+      addMarker({ lat: 38.877190, lng: -104.789808});
+      addMarker({ lat: 38.886107, lng: -104.775372});
+      addMarker({ lat: 38.893839, lng: -104.770232});
+      addMarker({ lat: 38.883307, lng: -104.754176});
+      addMarker({ lat: 38.869096, lng: -104.764380});
+      addMarker({ lat: 38.878603, lng: -104.769403});
+      addMarker({ lat: 38.871091, lng: -104.783582});
+      addMarker({ lat: 38.880245, lng: -104.744736});
+
+      function addMarker(coords){
+         const marker = new google.maps.Marker({
+          position: coords,
+          title:"Dog Park!",
+          map:map,
+          animation: google.maps.Animation.DROP,
+        });
+
+        const infowindow = new google.maps.InfoWindow({
+          content: "Dog Park is Here!",
+        });
+
+        marker.addListener("click", () => {
+          infowindow.open({
+            anchor: marker,
+            map,
+            shouldFocus: false,
+          });
+        });
+
+      }
+    }
